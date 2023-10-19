1f1a5ced3a90bed9f85a5f88ef4b0e7c39508718
agusnat <agustin.nat@gmail.com>
2023-10-19 19:45:49 -0300

    convertir numero negativo a positivo

--- assets/js/comprartickets.js
+++ assets/js/comprartickets.js
@@ -3,7 +3,7 @@ const inputCategoria = document.getElementById("inputCategoria");
 const inputCantidad = document.getElementById("inputCantidad");
 
 btnResumen.addEventListener("click",(e)=>{    
-    let precioTotal = 200 * inputCantidad.value;
+    let precioTotal = 200 * Math.abs(inputCantidad.value);
     let precioConDescuento = precioTotal - (precioTotal * (inputCategoria.value / 100));
 
     document.getElementById("precioTotal").innerText = precioConDescuento;
